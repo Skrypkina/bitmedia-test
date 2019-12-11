3030a201ecf26b9f705e0b590b48cb40c9ec5b39
Larysa <Larysa76@gmail.com>
2019-12-11 12:16:55 +0200

    Updates

---
+++ precache-manifest.7e58a37dcd8636cea37d30c8838a8672.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3d9e5f1bca180b2e04b9a2bbe934cf",
+    "url": "/bitmedia-test/index.html"
+  },
+  {
+    "revision": "899f5082f8562a1870e5",
+    "url": "/bitmedia-test/static/js/main.6c11cbbb.chunk.js"
+  },
+  {
+    "revision": "927847db6c1ef8b505cf",
+    "url": "/bitmedia-test/static/js/runtime-main.436838b3.js"
+  }
+]);
---
+++ static/js/main.6c11cbbb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbitmedia-test"]=this["webpackJsonpbitmedia-test"]||[]).push([[0],[function(t,i,s){t.exports=s(1)},function(t,i){}],[[0,1]]]);
+//# sourceMappingURL=main.6c11cbbb.chunk.js.map
