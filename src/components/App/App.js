ead14b6ec7224ff49ffc35cbc7728aec2964d43e
Larysa <Larysa76@gmail.com>
2020-01-04 19:05:42 +0200

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../../pages/Home';
 import StatsPage from '../../pages/Stats';
-import NotFound from '../../pages/NotFound';
+// import NotFound from '../../pages/NotFound';
 import UserStats from '../../pages/UserStats';
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
       <Route path="/" exact component={HomePage} />
       <Route path="/stats/:id" component={UserStats} />
       <Route path="/stats" component={StatsPage} />
-      {/* <Route component={NotFound} /> */}
+      <Route component={HomePage} />
     </Switch>
   );
 };
